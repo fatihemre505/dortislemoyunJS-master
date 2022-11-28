e73acf2adaaa339885f7fbc73b249a7b3a4d0ce8
fatihemre505 <fatihemre505@gmail.com>
2022-11-28 15:43:20 +0300

    ok case

---
+++ app.js
@@ -0,0 +1,40 @@
+function randomNumber(min, max) {
+    let number = Math.round(Math.random() * (max - min));
+    return number;
+}
+
+var number1, result, number2, operator, True = 0, False = 0;
+
+let selectOperator = ["+", "-", "X", "/"];
+
+let btncvp = document.getElementById("btncvp");
+
+window.onload = function () {
+
+    number1 = randomNumber(0, 100);
+    number2 = randomNumber(0, 5);
+    operator = selectOperator[3];
+    document.getElementById("operator").innerHTML = operator;
+    document.getElementById("number1").innerHTML = number1;
+    document.getElementById("number2").innerHTML = number2;
+
+    switch (operator) {
+        case "+":
+            result = number1 + number2
+            break;
+        case "-":
+            result = number1 - number2
+            break;
+        case "X":
+            result = number1 * number2
+            break;
+        case "/":
+            result = number1 / number2
+            break;
+        default:
+            result = "Geçersiz Seçim";
+    }
+
+    document.getElementById("result").innerHTML = result;
+
+}
